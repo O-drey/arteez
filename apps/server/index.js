18e4aa4e4f2f0b48046480c1e8fba3544a402490
Audrey Carmont <carmont.audrey@gmail.com>
2025-07-28 19:45:42 +0200

    init monorepo with turbo, cloudinary, react, express

---
+++ apps/server/index.js
@@ -0,0 +1,62 @@
+import express, { json, urlencoded } from "express"
+import createError from "http-errors"
+import { v2 as cloudinary } from "cloudinary"
+
+import usersRouter from "./routes/users.js"
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use("/users", usersRouter)
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+;(async function () {
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+  })
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(
+      "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+      {
+        public_id: "shoes",
+      }
+    )
+    .catch((error) => {
+      console.log(error)
+    })
+
+  console.log(uploadResult)
+
+  // Optimize delivery by resizing and applying auto-format and auto-quality
+  const optimizeUrl = cloudinary.url("shoes", {
+    fetch_format: "auto",
+    quality: "auto",
+  })
+
+  console.log(optimizeUrl)
+
+  // Transform the image: auto-crop to square aspect_ratio
+  const autoCropUrl = cloudinary.url("shoes", {
+    crop: "auto",
+    gravity: "auto",
+    width: 500,
+    height: 500,
+  })
+
+  console.log(autoCropUrl)
+})()
